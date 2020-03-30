15dc2adb10985bf15b5a0471c511b4c96cc8ad7e
Mr.TamfanX <62769928+Anonymous3-SIT@users.noreply.github.com>
2020-03-30 15:46:16 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# TOOLS BUCIN NEMBAK CEWEK
+
+
+#INSTALLATION
+- pkg update && pkg upgrade
+- pkg install python2
+- pkg install git
+- git clone https://github.com/Anonymous3-SIT/bucin
+- cd bucin
+- ls
+- sh bucin.sh
+
+
+#INFO
+- WhatsApp : 628999271792
+- GitHub   : https://github.com/Anonymous3-SIT
+- Website  : https://mraliefrega.low.blog
+-Gmail     : anonymoussit36@gmail.com
+
+
+#Copyright@2020
---
+++ bucin.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/bash
+#tools sederhana
+#bucin
+#by Mr.Tamfanx
+clear
+
+b='\033[34;1m' #blue
+g='\033[32;1m' #green
+c='\033[36;1m' #cyan
+r='\033[31;1m' #red
+w='\033[1;37m' #white
+
+
+echo "${r}"
+echo "  oo"                                       
+echo "     oo     OOOOOOOO:       OOOOOOOO!"       
+echo "        oOOOO!!!!::::O    OO.......::!O"     
+echo "       OOO!!!::::::::O  O.......:   : !O"    
+echo "       OOO!!!!::::::::.OO........:    :!O"  
+echo "       OO!!!!:::::::..............:   :!O"  
+echo "        OOO!!!:::::::..............:   :!O"  
+echo "        OO!!::::::::.............:   :!O"    
+echo "         OO!::::::::......oo.....::::!O"     
+echo "           O!!:::::::........oo..:::O"      
+echo "             !!!::::::..........ooO"         
+echo "                !!::::::.......O   oo"     
+echo "                  ::::::.....O        oo  .o"
+echo "                     :::..O              ooo"
+echo "                       ::.              oooo"
+echo "                        :"
+echo ""
+echo "              ${r}[  ${w}Hay Cantik  ${r}]"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${w}Siapa Nama kamu ?${c}"
+sleep 1
+read -p "     ┣Masukkan Nama┣==> " na;
+sleep 3
+echo ""
+echo "  ${r}[${w}+${r}] ${c}Hay $na Cantik"
+sleep 3
+echo ""
+echo "  ${r}[${w}+${r}] ${b}Wajahmu bagai sinar rembulan"
+sleep 3
+echo ""
+echo "  ${r}[${w}+${r}] ${r}Yang selalu menyinari hari hariku"
+sleep 3
+echo ""
+echo "  ${r}[${w}+${r}] ${w}Sejak pertama aku melihatmu"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${r}Betapa senangnya hatiku"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${b}Tapi rasa ini tak pernah sampai kehatimu"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${g}Dengan nyali dan lisan yang ku punya-_-"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${r}Dan kini tibalah saatnya aku ingin menggkapkan itu semua"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${b}Aku sayang padamu"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${w}Aku cinta padamu"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${g}Aku menyayangimu"
+sleep 1
+echo ""
+
+echo "  ${r}[${w}+${r}] ${r}Hadirmu kan menjadi penyemangat hidup ku"
+sleep 1
+echo ""
+echo "              ${r}[  ${g}MAUKAH KAMU MENJADI PACARKU ?  ${r}]"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${c}Jawablah dari perasaan hati yang terdalam ?${c}"
+sleep 1
+read -p "     ┣JAWABAN ${r} [${b} MAU ${c} / ${g} TIDAK ${r}] ${c}┣==> " jb;
+
+if [ $jb = MAU ] || [ $jb = mau ]
+then
+clear
+echo "  ${r}[${w}+${r}] ${b}Terimakasih kau mau menerimanya"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${g}Kamulah penyemangat hidupku"
+sleep 1
+echo ""
+sleep 2
+elif [ $jb = TIDAK ] || [ $jb = tidak ]
+then
+clear
+echo "  ${r}[${w}+${r}] ${b}Jika itu sudah menjadi keputusanmu"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${g}Aku rela menerimanya"
+sleep 1
+echo ""
+echo "  ${r}[${w}+${r}] ${b}Sebab, cinta sejati adalah kasih sayang tanpa ada paksaan"
+sleep 1
+echo ""
+fi
+
+
+    
